f5fc9f065114d9db587f5a1731b974435e94cf9d
Andrei Pata <andrei.pata@cleancodeculture.com>
2025-07-15 07:36:58 +0300

    Updates

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -148,7 +148,7 @@ export default function Header({ showTitle, title }: HeaderProps) {
       )}
 
       {/* Sidebar Menu */}
-      <div className={`fixed top-0 right-0 h-full w-[70%] bg-black transform rounded-l-3xl transition-transform duration-300 ease-in-out z-[9999] ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'
+      <div className={`fixed top-0 right-0 h-full w-[70%] bg-black transform rounded-bl-4xl transition-transform duration-300 ease-in-out z-[9999] ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'
         }`}>
         <div className="flex flex-col h-full">
           {/* Menu Items */}
